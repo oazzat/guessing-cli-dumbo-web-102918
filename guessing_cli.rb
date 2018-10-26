e400bfdc6ca4c0d0ca379b5925b502e0c9bb7b40
Omar A <oazzat19@gmail.com>
2018-10-26 21:37:56 +0000

    automatically backed up by learn

--- guessing_cli.rb
+++ guessing_cli.rb
@@ -3,14 +3,13 @@ require "pry"
 def run_guessing_game
   keepgoing = true
   while keepgoing
+  num = 1+ Random.rand(6)
   puts "Guess a number between 1 and 6."
   inp = gets.chomp
   
   if inp == "exit"
     puts "Goodbye!"
     keepgoing = false
-  else
-    num = 1+ Random.rand(6)
   end
     
     if inp.to_i == num
